f08f74d2dc544c9b289f09d7911c43534900f8c2
braniel pichardo <DonPeri@braniels-Mac-mini.local>
2022-04-06 17:17:52 -0400

    update api endpoint

--- Assets/app.js
+++ Assets/app.js
@@ -1,42 +1,31 @@
 
 var sports = ['Drift', 'Nascar', 'Formula One'];
+var config = "/config.js/";
 
 
 function motorSpotGifs() {
     var mSport = $(this).attr('data-name');
     var apiKey = config.SECRET_API_KEY;
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + mSport + '&rating=pg-13&' + apiKey;
-    //gets the data from the api
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + mSport + '&rating=pg-13&api_key=' + apiKey;
     $.ajax({
             url: queryURL,
             method: "GET",
         })
-        //waits until the url loads before doing anything 
         .done(function(response) {
-            //empties the div berfore populating it with new gifs 
+            
             $('#images').empty();
-            //store the data obtained from the API in the results variable 
             var results = response.data;
-
-            //looping through the gifs obtained from the API
             for (j = 0; j < results.length; j++) {
                 //creates a new div
                 var gifContainer = $('div');
-                //stores the ratings of the gifs in the rating variable
                 var rating = results[j].rating;
-                //stores the link to the still gif
                 var still = results[j].images.original_still.url;
-                //stores the link to the animated gif
                 var animated = results[j].images.original.url;
-                //creates and store the img tags and other attributes in the sportsGif variable
                 var sportsGif = $('<img>').addClass('thisGif').attr('src', still).attr('data-state', 'still').attr('data-still', still).attr('data-animate', animated);
-                //sotres the the ratings and assigns them a proper tag
                 var p = $('<p>').text('Rating: ' + rating);
-                //gifs and ratings are appended to the corresponding div
                 $('#images').append(sportsGif).append(p);
             }
-            //event listener that will switch the gifs form still to animate
-            //and vise versa everytime on of them is clicked 
+
             $('.thisGif').on('click', function() {
 
                 var state = $(this).attr('data-state');
